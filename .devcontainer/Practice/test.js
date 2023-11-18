a867866fc141d654ca9dfc1a55ff03094c759844
shanukumarsingh <90618356+shanukumarsingh@users.noreply.github.com>
2023-11-18 18:46:45 +0000

    Lernt const,var,let

--- .devcontainer/Practice/test.js
+++ .devcontainer/Practice/test.js
@@ -1,2 +1,24 @@
-console.log("shanu")
-console.log("practice")
+
+const accountId=1088488 //const we de c
+let accountEmail="shanu@gmail.com"
+var accontPassword="12345"
+accountCity="jaipur"
+let accountState;
+
+// accountId=20; // not allowed re declerae as it is constant it will throw an error 
+console.log(accountId);
+console.log(accountEmail);
+console.log(accontPassword);
+console.log(accountCity);
+accountEmail="@gamail.com"
+accontPassword="@123fhh"
+accountCity="benguluru"
+console.log(accountEmail);
+console.table([accontPassword,accountEmail,accontPassword,accountCity,accountState]);
+
+/*
+Prefer not to use var
+Because of issue in a block scope and function
+*/
+
+{}//scope 
